8a927bcf7d414aa3a1414768fd354c6ffa46105e
Jonathan (JB) Belcher <belcherj@gmail.com>
2019-12-17 15:31:53 -0500

    progress commit

--- lib/note-toolbar/index.jsx
+++ lib/note-toolbar/index.jsx
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { noop } from 'lodash';
-
 import IconButton from '../icon-button';
 import BackIcon from '../icons/back';
 import InfoIcon from '../icons/info';
@@ -10,65 +8,30 @@ import PreviewStopIcon from '../icons/preview-stop';
 import RevisionsIcon from '../icons/revisions';
 import TrashIcon from '../icons/trash';
 import ShareIcon from '../icons/share';
+import { showRevisionPanel } from '../state/revision/actions';
 import SidebarIcon from '../icons/sidebar';
+import { useDispatch } from 'react-redux';
 
-export class NoteToolbar extends Component {
-  static displayName = 'NoteToolbar';
+const NoteToolbar = ({
+  editorMode,
+  onCloseNote,
+  onDeleteNoteForever,
+  onRestoreNote,
+  onSetEditorMode,
+  onShareNote,
+  onShowNoteInfo,
+  onTrashNote,
+  markdownEnabled,
+  note,
+  toggleFocusMode,
+}) => {
+  const dispatch = useDispatch();
 
-  static propTypes = {
-    note: PropTypes.object,
-    onRestoreNote: PropTypes.func,
-    onTrashNote: PropTypes.func,
-    onDeleteNoteForever: PropTypes.func,
-    onShowRevisions: PropTypes.func,
-    onShareNote: PropTypes.func,
-    onCloseNote: PropTypes.func,
-    onShowNoteInfo: PropTypes.func,
-    setIsViewingRevisions: PropTypes.func,
-    toggleFocusMode: PropTypes.func.isRequired,
-    onSetEditorMode: PropTypes.func,
-    editorMode: PropTypes.string,
-    markdownEnabled: PropTypes.bool,
+  const setEditorMode = () => {
+    onSetEditorMode(editorMode === 'markdown' ? 'edit' : 'markdown');
   };
 
-  static defaultProps = {
-    editorMode: 'edit',
-    onCloseNote: noop,
-    onDeleteNoteForever: noop,
-    onRestoreNote: noop,
-    onSetEditorMode: noop,
-    onShowNoteInfo: noop,
-    onShowRevisions: noop,
-    onShareNote: noop,
-    onTrashNote: noop,
-    setIsViewingRevisions: noop,
-    toggleFocusMode: noop,
-  };
-
-  showRevisions = () => {
-    this.props.setIsViewingRevisions(true);
-    this.props.onShowRevisions(this.props.note);
-  };
-
-  render() {
-    const { note } = this.props;
-    const isTrashed = !!(note && note.data.deleted);
-
-    return (
-      <div className="note-toolbar-wrapper theme-color-border">
-        {isTrashed ? this.renderTrashed() : this.renderNormal()}
-      </div>
-    );
-  }
-
-  setEditorMode = () => {
-    const { editorMode } = this.props;
-
-    this.props.onSetEditorMode(editorMode === 'markdown' ? 'edit' : 'markdown');
-  };
-
-  renderNormal = () => {
-    const { note, editorMode, markdownEnabled } = this.props;
+  const renderNormal = () => {
     const isPreviewing = editorMode === 'markdown';
 
     return !note ? (
@@ -79,7 +42,7 @@ export class NoteToolbar extends Component {
           <div className="note-toolbar__button">
             <IconButton
               icon={<SidebarIcon />}
-              onClick={this.props.toggleFocusMode}
+              onClick={toggleFocusMode}
               title="Toggle Sidebar"
             />
           </div>
@@ -88,7 +51,7 @@ export class NoteToolbar extends Component {
           <div className="note-toolbar__button note-toolbar-back">
             <IconButton
               icon={<BackIcon />}
-              onClick={this.props.onCloseNote}
+              onClick={onCloseNote}
               title="Back"
             />
           </div>
@@ -96,7 +59,7 @@ export class NoteToolbar extends Component {
             <div className="note-toolbar__button">
               <IconButton
                 icon={isPreviewing ? <PreviewStopIcon /> : <PreviewIcon />}
-                onClick={this.setEditorMode}
+                onClick={setEditorMode}
                 title="Preview • Ctrl+Shift+P"
               />
             </div>
@@ -104,28 +67,28 @@ export class NoteToolbar extends Component {
           <div className="note-toolbar__button">
             <IconButton
               icon={<RevisionsIcon />}
-              onClick={this.showRevisions}
+              onClick={() => dispatch(showRevisionPanel())}
               title="History"
             />
           </div>
           <div className="note-toolbar__button">
             <IconButton
               icon={<ShareIcon />}
-              onClick={this.props.onShareNote.bind(null)}
+              onClick={onShareNote.bind(null)}
               title="Share"
             />
           </div>
           <div className="note-toolbar__button">
             <IconButton
               icon={<TrashIcon />}
-              onClick={this.props.onTrashNote.bind(null, note)}
+              onClick={onTrashNote.bind(null, note)}
               title="Trash"
             />
           </div>
           <div className="note-toolbar__button">
             <IconButton
               icon={<InfoIcon />}
-              onClick={this.props.onShowNoteInfo}
+              onClick={onShowNoteInfo}
               title="Info"
             />
           </div>
@@ -134,24 +97,18 @@ export class NoteToolbar extends Component {
     );
   };
 
-  renderTrashed = () => {
-    const { note } = this.props;
-
+  const renderTrashed = () => {
     return (
       <div className="note-toolbar-trashed">
         <div className="note-toolbar__column-left">
-          <IconButton
-            icon={<BackIcon />}
-            onClick={this.props.onCloseNote}
-            title="Back"
-          />
+          <IconButton icon={<BackIcon />} onClick={onCloseNote} title="Back" />
         </div>
         <div className="note-toolbar__column-right">
           <div className="note-toolbar__button">
             <button
               type="button"
               className="button button-compact button-danger"
-              onClick={this.props.onDeleteNoteForever.bind(null, note)}
+              onClick={onDeleteNoteForever.bind(null, note)}
             >
               Delete Forever
             </button>
@@ -160,7 +117,7 @@ export class NoteToolbar extends Component {
             <button
               type="button"
               className="button button-primary button-compact"
-              onClick={this.props.onRestoreNote.bind(null, note)}
+              onClick={onRestoreNote.bind(null, note)}
             >
               Restore Note
             </button>
@@ -169,6 +126,28 @@ export class NoteToolbar extends Component {
       </div>
     );
   };
-}
+
+  return (
+    <div className="note-toolbar-wrapper theme-color-border">
+      {note && note.data.deleted ? renderTrashed() : renderNormal()}
+    </div>
+  );
+};
+
+NoteToolbar.propTypes = {
+  note: PropTypes.object,
+  onRestoreNote: PropTypes.func,
+  onTrashNote: PropTypes.func,
+  onDeleteNoteForever: PropTypes.func,
+  onShowRevisions: PropTypes.func,
+  onShareNote: PropTypes.func,
+  onCloseNote: PropTypes.func,
+  onShowNoteInfo: PropTypes.func,
+  setIsViewingRevisions: PropTypes.func,
+  toggleFocusMode: PropTypes.func,
+  onSetEditorMode: PropTypes.func,
+  editorMode: PropTypes.string,
+  markdownEnabled: PropTypes.bool,
+};
 
 export default NoteToolbar;
